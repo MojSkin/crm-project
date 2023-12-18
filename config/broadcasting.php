9945fb9546ec425e991ab9a04e0d70b503a56a91
MojSkin <mojtaba.meskin@gmail.com>
2023-12-18 15:59:36 +0330

    WebSocket use only WS

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,8 +40,8 @@ return [
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => ['http', 'https'],
-                'encrypted' => true,
-                'useTLS' => false,
+                'encrypted' => false,
+                'useTLS' => true,
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
