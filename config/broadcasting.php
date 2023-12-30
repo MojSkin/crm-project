b41f731c08f9c9edf9821c0218e7dbc4dc4f25f5
MojSkin <mojtaba.meskin@gmail.com>
2023-12-30 14:05:34 +0330

    Work on why Socket is not work

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -44,8 +44,8 @@ return [
                 'scheme' => env('PUSHER_SCHEME', 'http'),
                 'debug' => true,
                 'curl_options' => [
-                    CURLOPT_SSL_VERIFYPEER => 0,
                     CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
                 ]
             ],
         ],
